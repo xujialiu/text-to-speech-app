a428b3bc4ba2287fed1bcc02c6a4a50a3240162e
xujialiu <xujialiuphd@gmail.com>
2024-07-16 02:49:39 +0800

    chore: modify main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import multiprocessing
 import sys
 from PySide6.QtWidgets import QApplication
 from MainWindowImpl import MainWindowImpl
 
+
+
+multiprocessing.freeze_support()  # This is necessary for Windows
 app = QApplication(sys.argv)
-mwImpl = MainWindowImpl()
-mwImpl.show()
+window = MainWindowImpl()
+window.show()
 sys.exit(app.exec())
